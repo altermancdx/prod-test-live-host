e024043035b24d65590798c5d95e46559d354b21
alterman <caodx@citydo.com.cn>
2020-10-22 12:24:04 +0800

    add dog widget

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DogWidget from './DogWidget';
+
+import "./index.css";
+
+const App = () => <div><DogWidget /></div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
